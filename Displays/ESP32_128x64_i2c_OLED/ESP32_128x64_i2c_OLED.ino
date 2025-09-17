c78fab465e907d25e8e793fd4197f5218de757e6
Scott Fitzgerald <scott@droolcup.com>
2025-09-17 09:50:39 -0400

    update i2c screen

--- Displays/ESP32_128x64_i2c_OLED/ESP32_128x64_i2c_OLED.ino
+++ Displays/ESP32_128x64_i2c_OLED/ESP32_128x64_i2c_OLED.ino
@@ -3,14 +3,14 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
-// on esp32 nano it's SCL on A5 and sda on a4
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address
+// The pins for I2C are defined by the Wire-library.
+// on esp32 nano it's SCL on A5 and SDA on a4
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const int sensorPin = A0;
@@ -18,51 +18,57 @@ int sensorVal = 0;
 
 
 void setup() {
-  Serial.begin(9600);
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     /// wait to init
   }
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-
-
-  // Clear the buffer
+  // Clear the display buffer
   display.clearDisplay();
 
   // draw a rectangle
   display.drawRect(0, 0, 128, 16, SSD1306_WHITE);
-
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
+  // move the cursor
   display.setCursor(4, 4);
+  // set the text color
   display.setTextColor(WHITE, BLACK);
-  display.print("Hello class");
+  // write some text
+    display.print("Hello class");
+  // update the display
   display.display();
-  delay(5000);
+  delay(1000);
   // display.display() is NOT necessary after every single drawing command,
   // unless that's what you want...rather, you can batch up a bunch of
   // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
+  // display.display().
 
+  // some static text
+  display.drawRect(0, 0, 128, 16, SSD1306_WHITE);
+  display.setCursor(4, 4);
+  display.setTextColor(WHITE, BLACK);
+  display.setTextSize(1);
+  display.print("Sensor Reading");
+  display.setCursor(4, 20);
+  display.setTextSize(2);
+  display.print("Value:");
 }
 
 void loop() {
   // read the value of the sensor
   sensorVal = analogRead(sensorPin);
 
-  // print it out to the screen!
-  display.drawRect(0, 0, 128, 16, SSD1306_WHITE);
-  display.setCursor(4, 4);
-  display.setTextColor(WHITE, BLACK);
-  display.print("Sensor Reading");
-
-  display.setCursor(4, 20);
-  display.setTextSize(2);
-  display.print("Value : ");
+  // move the cursor to the location
+  display.setCursor(75, 20);
+  // erase what was there previously
+  display.print("   ");
+  // move the cursor back to position
+  display.setCursor(75, 20);
+  // print the sensor value
   display.print(sensorVal);
-  display.display();
+
+  // down here you could display a graph of the last 128 sensor readings
+
+  display.display(); // update the display
+  // slight delay - 16ms == 60fps
   delay(16);
 }
-
